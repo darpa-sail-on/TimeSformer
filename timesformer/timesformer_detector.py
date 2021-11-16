ef83dc3852369d8443e60d3dedafde22c15601fb
ameya.shringi <ameya.shringi@kitware.com>
2021-11-16 16:26:35 -0500

    Update agent and config

--- timesformer/timesformer_detector.py
+++ timesformer/timesformer_detector.py
@@ -1,20 +1,24 @@
+import copy
 import logging
+import numpy as np
 import os
-from fvcore.common.config import CfgNode
 import pandas as pd
 import torch
+
+from fvcore.common.config import CfgNode
+from vast.opensetAlgos.extreme_value_machine import ExtremeValueMachine
 
 import timesformer.utils.checkpoint as cu
 from timesformer.models import build_model
 from timesformer.datasets.ta2 import TimesformerEval
-import timesformer.utils.checkpoint as cu
 from timesformer.config.defaults import get_cfg
 
 
 class TimesformerDetector:
     def __init__(self, session_id, test_id, test_type,
                  feature_extractor_params, kl_params, evm_params,
-                 characterization_params, dataloader_params):
+                 characterization_params, dataloader_params,
+                 detection_threshold):
         """
         Constructor for the activity recognition models
 
@@ -25,6 +29,7 @@ class TimesformerDetector:
         :param kl_params (dict): Parameter for kl divergence
         :param evm_params (dict): Parameter for evm
         :param dataloader_params (dict): Parameters for dataloader
+        :param detection_threshold (float): The threshold for binary detection
         """
         self.logger = logging.getLogger(__name__)
         self.session_id = session_id
@@ -53,10 +58,27 @@ class TimesformerDetector:
         self.base_cfg.DATA.TRAIN_JITTER_SCALES = dataloader_params["train_jitter_scales"]
         self.base_cfg.DATA.SAMPLING_RATE = dataloader_params["sampling_rate"]
         self.base_cfg.DATA.NUM_FRAMES = dataloader_params["num_frames"]
-
         self.batch_size = dataloader_params["batch_size"]
         self.number_workers = dataloader_params["n_threads"]
         self.pin_memory = dataloader_params["pin_memory"]
+
+        # Add KL parameters
+        self.kl_threshold = kl_params["KL_threshold"]
+        kl_decay_rate = kl_params["decay_rate"]
+        num_rounds = kl_params["num_rounds"]
+        self.sliding_window = []
+        self.past_window = []
+        self.window_size = kl_params["window_size"]
+        self.sigma_train = kl_params["sigma_train"]
+        self.mu_train = kl_params["mu_train"]
+        self.acc = 0.0
+        self.has_world_changed = False
+        self.kl_threshold_decay = (num_rounds  * kl_decay_rate)/float(num_rounds)
+
+        self.detection_threshold = detection_threshold
+        self.evm = ExtremeValueMachine.load(evm_params["model_path"],
+                                            device="cuda:0")
+        self.logger.info(f"{self.logging_header}: Initialization complete")
 
     @torch.no_grad()
     def feature_extraction(self, dataset_path, dataset_root, round_id=None):
@@ -95,3 +117,137 @@ class TimesformerDetector:
 
         self.logger.info(f"{self.logging_header}: Finished feature extraction")
         return feature_dict, logit_dict
+
+    def kullback_leibler(self, mu, sigma, m, s):
+        """
+        Compute Kullback Leibler with Gaussian assumption of training data
+        mu: mean of test batch
+        sigm: standard deviation of test batch
+        m: mean of all data in training data set
+        s: standard deviation of all data in training data set
+        return: KL distance, non negative double precison float
+        """
+        kl = torch.log(s/sigma) + (((sigma ** 2) + ((mu - m) ** 2)) / (2 * (s ** 2))) - 0.5
+        return kl
+
+    def world_detection(self, feature_dict, logit_dict, round_id=None):
+        """
+        Detect Change in World
+
+        :param feature_dict (dict): Dictionary containing features
+        :param logit_dict   (dict): Dictionary containing logits
+        :param round_id     (int): Integer identifier for round
+
+        :return string containing path to csv file with the results
+        """
+        logging_header = self._add_round_to_header(self.logging_header, round_id)
+        self.logger.info(f"{logging_header}: Starting to detect change in world")
+        if round_id is None:
+            result_path = f"wd_{self.session_id}_{self.test_id}.csv"
+        else:
+            result_path = f"wd_{self.session_id}_{self.test_id}_{round_id}.csv"
+        image_names, FVs = zip(*feature_dict.items())
+        class_map = map(self.evm.known_probs,
+                        map(lambda x: torch.Tensor(x).double(), FVs))
+        self.class_probabilities = torch.stack(list(class_map), axis=0)
+        self.max_probabilities, _ = torch.max(self.class_probabilities, axis=2)
+        mean_max_probs = torch.mean(self.max_probabilities, axis=1)
+        round_size = len(feature_dict)
+        # Populate sliding window
+        self.past_window = copy.deepcopy(self.sliding_window)
+        self.sliding_window.extend(mean_max_probs)
+        if len(self.sliding_window) >= self.window_size:
+            window_size = len(self.sliding_window)
+            self.sliding_window = \
+                    self.sliding_window[window_size-self.window_size:]
+
+        if len(self.sliding_window) < self.window_size or round_id == 0:
+            df = pd.DataFrame(zip(image_names, [0.0]*len(image_names)))
+        else:
+            # Redundant case when acc is 1.0
+            if self.acc == 1.0:
+                df = pd.DataFrame(zip(image_names, [1.0]*len(image_names)))
+            else:
+                # Using kl divergence
+                with torch.no_grad():
+                    self.past_p = torch.Tensor(self.past_window)
+                    self.current_p = torch.Tensor(self.sliding_window)
+                    self.temp_world_changed = torch.zeros(round_size)
+                    p_past_and_current = torch.cat((self.past_p[1:],
+                                                    self.current_p))
+                    p_window = p_past_and_current.unfold(0, self.window_size, 1)
+                    mu = torch.mean(p_window, dim=1)
+                    sigma = torch.std(p_window, dim=1)
+                    kl_epoch = self.kullback_leibler(mu, sigma, self.mu_train,
+                                                     self.sigma_train)
+                    self.logger.info(f"max kl_epoch = {torch.max(kl_epoch)}")
+                    W = (kl_epoch / (2*self.kl_threshold))#1.1
+                    logging.info(f"W = {W.tolist()}")
+                    W[0] = torch.clamp(W[0], min=self.acc)
+                    W , _ = torch.cummax (W, dim=0)
+                    self.temp_world_changed = \
+                            torch.clamp(W , max=1.0)[len(W)-round_size:]
+                    self.temp_world_changed = torch.clamp(self.temp_world_changed,
+                                                          min=0)
+                    approx_world_changed = \
+                            list(np.around(self.temp_world_changed.detach().numpy(), 4))
+                    self.logger.info(f"self.temp_world_changed = {approx_world_changed}")
+                    self.acc = self.temp_world_changed[-1]
+                    df = pd.DataFrame(zip(image_names,
+                                          self.temp_world_changed.tolist()),
+                                      columns=['id', 'P_world_changed'])
+                if self.acc > self.detection_threshold:
+                    self.has_world_changed = True
+        self.logger.info(f"{logging_header}: Number of samples in results {df.shape}")
+        df.to_csv(result_path, index=False, header=False, float_format='%.4f')
+        self.logger.info(f"{logging_header}: Finished with change detection")
+        self.kl_threshold =  self.kl_threshold - self.kl_threshold_decay
+        return result_path
+
+    def novelty_classification(self, feature_dict, logit_dict, round_id=None):
+        logging_header = self._add_round_to_header(self.logging_header, round_id)
+        self.logger.info(f"{logging_header}: Starting to classify samples")
+        image_names, FVs = zip(*feature_dict.items())
+        self.image_names = list(image_names)
+        if not hasattr(self, "class_probabilities"):
+            class_map = map(self.evm.class_probabilities, FVs)
+            self.class_probabilities = torch.stack(list(class_map), axis=0)
+            self.max_probabilities, _ = torch.max(self.class_probabilities, axis=2)
+        m = 1 - torch.mean(self.max_probabilities, axis=1)
+        known_probs = torch.mean(torch.tensor(self.class_probabilities), axis=1)
+        pu = torch.zeros(m.shape)
+        class_logits = []
+        for _, logit in logit_dict.items():
+            class_logit = torch.Tensor(logit["class_preds"])
+            class_logits.append(class_logit)
+        logits_tensor = torch.stack(class_logits).cpu()
+        softmax_scores = torch.nn.functional.softmax(logits_tensor, dim=1)
+        softmax_scores = torch.mean(softmax_scores, axis=1)
+        self.logger.info(f"Softmax scores: {torch.argmax(softmax_scores, dim=1)}")
+        self.logger.info(f"EVM scores: {torch.argmax(known_probs, dim=1)}")
+        self.logger.info(f"Acc: {self.acc}")
+        if self.has_world_changed:
+            scaled_m = torch.ones(m.shape).double()
+            scaled_m[m >= self.detection_threshold] = \
+                (m[m >= self.detection_threshold] - 0.001)
+            scaled_softmax = torch.einsum("ij,i->ij", softmax_scores, scaled_m)
+            all_rows_tensor = torch.cat((scaled_softmax, m.view(-1, 1)), 1)
+        else:
+            pu = pu.view(-1, 1)
+            all_rows_tensor = torch.cat((softmax_scores, pu), 1)
+        norm = torch.norm(all_rows_tensor, p=1, dim=1)
+        normalized_tensor = all_rows_tensor/norm[:, None]
+        df = pd.DataFrame(zip(image_names, *normalized_tensor.t().tolist()))
+        if round_id is None:
+            result_path = f"ncl_{self.session_id}_{self.test_id}.csv"
+        else:
+            result_path = f"ncl_{self.session_id}_{self.test_id}_{round_id}.csv"
+        df.to_csv(result_path, index = False, header = False, float_format='%.4f')
+        self.logger.info(f"{logging_header}: Finished classifying samples")
+        return result_path
+
+    def _add_round_to_header(self, logger_header, round_id):
+        if round_id is not None:
+            logger_header += " Round id: {}".format(round_id)
+        return logger_header
+
