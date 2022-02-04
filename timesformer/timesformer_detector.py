89fc06384140b076a569936c7c1dcecad1d6774b
ameya.shringi <ameya.shringi@kitware.com>
2022-02-04 11:38:22 -0500

    Refactor out kl divergence computation

--- timesformer/timesformer_detector.py
+++ timesformer/timesformer_detector.py
@@ -12,6 +12,7 @@ import timesformer.utils.checkpoint as cu
 from timesformer.models import build_model
 from timesformer.datasets.ta2 import TimesformerEval
 from timesformer.config.defaults import get_cfg
+from timesformer.kl_divergence import kl_divergence_based_wd
 from timesformer.utils.realign_logits import realign_logits
 
 
@@ -122,18 +123,6 @@ class TimesformerDetector:
         self.logger.info(f"{self.logging_header}: Finished feature extraction")
         return feature_dict, logit_dict
 
-    def kullback_leibler(self, mu, sigma, m, s):
-        """
-        Compute Kullback Leibler with Gaussian assumption of training data
-        mu: mean of test batch
-        sigm: standard deviation of test batch
-        m: mean of all data in training data set
-        s: standard deviation of all data in training data set
-        return: KL distance, non negative double precison float
-        """
-        kl = torch.log(s/sigma) + (((sigma ** 2) + ((mu - m) ** 2)) / (2 * (s ** 2))) - 0.5
-        return kl
-
     def world_detection(self, feature_dict, logit_dict, round_id=None):
         """
         Detect Change in World
@@ -151,57 +140,23 @@ class TimesformerDetector:
         else:
             result_path = f"wd_{self.session_id}_{self.test_id}_{round_id}.csv"
         image_names, FVs = zip(*feature_dict.items())
-        class_map = map(self.evm.known_probs,
-                        map(lambda x: torch.Tensor(x).double(), FVs))
-        self.class_probabilities = torch.stack(list(class_map), axis=0)
-        self.max_probabilities, _ = torch.max(self.class_probabilities, axis=2)
-        mean_max_probs = torch.mean(self.max_probabilities, axis=1)
-        round_size = len(feature_dict)
-        # Populate sliding window
-        self.past_window = copy.deepcopy(self.sliding_window)
-        self.sliding_window.extend(mean_max_probs)
-        if len(self.sliding_window) >= self.window_size:
-            window_size = len(self.sliding_window)
-            self.sliding_window = \
-                    self.sliding_window[window_size-self.window_size:]
-
-        if len(self.sliding_window) < self.window_size or round_id == 0:
-            df = pd.DataFrame(zip(image_names, [0.0]*len(image_names)))
+        if self.has_world_changed:
+            image_predictions =  [1.0]*len(image_names)
         else:
-            # Redundant case when acc is 1.0
-            if self.acc == 1.0:
-                df = pd.DataFrame(zip(image_names, [1.0]*len(image_names)))
-            else:
-                # Using kl divergence
-                with torch.no_grad():
-                    self.past_p = torch.Tensor(self.past_window)
-                    self.current_p = torch.Tensor(self.sliding_window)
-                    self.temp_world_changed = torch.zeros(round_size)
-                    p_past_and_current = torch.cat((self.past_p[1:],
-                                                    self.current_p))
-                    p_window = p_past_and_current.unfold(0, self.window_size, 1)
-                    mu = torch.mean(p_window, dim=1)
-                    sigma = torch.std(p_window, dim=1)
-                    kl_epoch = self.kullback_leibler(mu, sigma, self.mu_train,
-                                                     self.sigma_train)
-                    self.logger.info(f"max kl_epoch = {torch.max(kl_epoch)}")
-                    W = (kl_epoch / (2*self.kl_threshold))#1.1
-                    logging.info(f"W = {W.tolist()}")
-                    W[0] = torch.clamp(W[0], min=self.acc)
-                    W , _ = torch.cummax (W, dim=0)
-                    self.temp_world_changed = \
-                            torch.clamp(W , max=1.0)[len(W)-round_size:]
-                    self.temp_world_changed = torch.clamp(self.temp_world_changed,
-                                                          min=0)
-                    approx_world_changed = \
-                            list(np.around(self.temp_world_changed.detach().numpy(), 4))
-                    self.logger.info(f"self.temp_world_changed = {approx_world_changed}")
-                    self.acc = self.temp_world_changed[-1]
-                    df = pd.DataFrame(zip(image_names,
-                                          self.temp_world_changed.tolist()),
-                                      columns=['id', 'P_world_changed'])
-                if self.acc > self.detection_threshold:
-                    self.has_world_changed = True
+            class_map = map(self.evm.known_probs,
+                            map(lambda x: torch.Tensor(x).double(), FVs))
+            self.class_probabilities = torch.stack(list(class_map), axis=0)
+            self.max_probabilities, _ = torch.max(self.class_probabilities, axis=2)
+            mean_max_probs = torch.mean(self.max_probabilities, axis=1)
+            image_predictions, self.sliding_window = kl_divergence_based_wd(
+                    mean_max_probs, self.sliding_window, self.window_size,
+                    self.mu_train, self.sigma_train, self.kl_threshold,
+                    self.acc)
+            self.acc = image_predictions[-1]
+            if self.acc > self.detection_threshold:
+                self.has_world_changed = True
+        df = pd.DataFrame(zip(image_names, image_predictions),
+                          columns=['id', 'P_world_changed'])
         self.logger.info(f"{logging_header}: Number of samples in results {df.shape}")
         df.to_csv(result_path, index=False, header=False, float_format='%.4f')
         self.logger.info(f"{logging_header}: Finished with change detection")
@@ -225,7 +180,6 @@ class TimesformerDetector:
         realigned_logits = realign_logits(logits_tensor)
         class_probabilities = realign_logits(self.class_probabilities)
         m = 1 - torch.mean(self.max_probabilities, axis=1)
-
         known_probs = torch.mean(torch.tensor(class_probabilities), axis=1)
         pu = torch.zeros(m.shape)
         logits_tensor = torch.sum(realigned_logits, dim=1)
@@ -257,4 +211,3 @@ class TimesformerDetector:
         if round_id is not None:
             logger_header += " Round id: {}".format(round_id)
         return logger_header
-
